f6743b44ba724500b6bf4c8c64873669053c8fba
Supanat <supanat.ji@gmail.com>
2019-01-02 05:39:12 +0700

    update

--- grid.py
+++ grid.py
@@ -1,4 +1,7 @@
 import operator
+import random
+from collections import defaultdict
+
 
 #Define Helper Method
 
@@ -28,6 +31,24 @@ def go(state, direction):
     
 def vector_add(a, b):
     return tuple(map(operator.add, a, b))
+
+identity = lambda x: x
+
+argmin = min
+argmax = max
+
+
+def shuffled(iterable):
+    """Randomly shuffle a copy of iterable."""
+    items = list(iterable)
+    random.shuffle(items)
+    return items
+
+
+def argmax_random_tie(seq, key=identity):
+    """Return an element with highest fn(seq[i]) score; break ties at random."""
+    return argmax(shuffled(seq), key=key)
+
 
 
 # Define MDP Class
@@ -171,4 +192,113 @@ class Grid(MDP):
     def to_arrows(self, policy):
         chars = {(1, 0): '>', (0, 1): '^', (-1, 0): '<', (0, -1): 'v', None: '.'}
         return self.to_grid({s: chars[a] for (s, a) in policy.items()})
+
+class QLearningAgent:
+    """ An exploratory Q-learning agent. It avoids having to learn the transition
+        model because the Q-value of a state can be related directly to those of
+        its neighbors. [Figure 21.8]
+
+    import sys
+    from mdp import sequential_decision_environment
+    north = (0, 1)
+    south = (0,-1)
+    west = (-1, 0)
+    east = (1, 0)
+    policy = {(0, 2): east, (1, 2): east, (2, 2): east, (3, 2): None, (0, 1): north, (2, 1): north, (3, 1): None, (0, 0): north, (1, 0): west, (2, 0): west, (3, 0): west,}
+    q_agent = QLearningAgent(sequential_decision_environment, Ne=5, Rplus=2, alpha=lambda n: 60./(59+n))
+    for i in range(200):
+        run_single_trial(q_agent,sequential_decision_environment)
+    
+    q_agent.Q[((0, 1), (0, 1))] >= -0.5
+    True
+    q_agent.Q[((1, 0), (0, -1))] <= 0.5
+    True
+    """
+    def __init__(self, mdp, Ne, Rplus, alpha=None):
+
+        self.gamma = mdp.gamma
+        self.terminals = mdp.terminals
+        self.all_act = mdp.actlist
+        self.Ne = Ne  # iteration limit in exploration function
+        self.Rplus = Rplus  # large value to assign before iteration limit
+        self.Q = defaultdict(float)
+        self.Nsa = defaultdict(float)
+        self.s = None
+        self.a = None
+        self.r = None
+
+        if alpha:
+            self.alpha = alpha
+        else:
+            self.alpha = lambda n: 1./(1+n)  # udacity video
+
+    def f(self, u, n):
+        """ Exploration function. Returns fixed Rplus until
+        agent has visited state, action a Ne number of times.
+        Same as ADP agent in book."""
+        if n < self.Ne:
+            return self.Rplus
+        else:
+            return u
+
+    def actions_in_state(self, state):
+        """ Return actions possible in given state.
+            Useful for max and argmax. """
+        if state in self.terminals:
+            return [None]
+        else:
+            return self.all_act
+
+    def __call__(self, percept):
+        s1, r1 = self.update_state(percept)
+        Q, Nsa, s, a, r = self.Q, self.Nsa, self.s, self.a, self.r
+        alpha, gamma, terminals = self.alpha, self.gamma, self.terminals,
+        actions_in_state = self.actions_in_state
+
+        if s in terminals:
+            Q[s, None] = r1
+        if s is not None:
+            Nsa[s, a] += 1
+            Q[s, a] += alpha(Nsa[s, a]) * (r + gamma * max(Q[s1, a1]
+                                           for a1 in actions_in_state(s1)) - Q[s, a])
+        if s in terminals:
+            self.s = self.a = self.r = None
+        else:
+            self.s, self.r = s1, r1
+            self.a = argmax(actions_in_state(s1), key=lambda a1: self.f(Q[s1, a1], Nsa[s1, a1]))
+        return self.a
+
+    def update_state(self, percept):
+        """To be overridden in most cases. The default case
+        assumes the percept to be of type (state, reward)."""
+        return percept
+
+
+def run_single_trial(agent_program, mdp):
+    """Execute trial for given agent_program
+    and mdp. mdp should be an instance of subclass
+    of mdp.MDP """
+
+    def take_single_action(mdp, s, a):
+        """
+        Select outcome of taking action a
+        in state s. Weighted Sampling.
+        """
+        x = random.uniform(0, 1)
+        cumulative_probability = 0.0
+        for probability_state in mdp.T(s, a):
+            probability, state = probability_state
+            cumulative_probability += probability
+            if x < cumulative_probability:
+                break
+        return state
+
+    current_state = mdp.init
+    while True:
+        current_reward = mdp.R(current_state)
+        percept = (current_state, current_reward)
+        next_action = agent_program(percept)
+        if next_action is None:
+            break
+        current_state = take_single_action(mdp, current_state, next_action)
 
