aa194675b05924f052239cb3f1d60624b631c80f
Supanat <supanat.ji@gmail.com>
2019-01-01 17:38:51 +0700

    define helper

---
+++ grid.py
@@ -0,0 +1,174 @@
+import operator
+
+#Define Helper Method
+
+orientations = EAST, NORTH, WEST, SOUTH = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+turns = LEFT, RIGHT = (+1, -1)
+
+def calculate_T(state, action):
+        if action:
+            return [(0.8, go(state, action)),
+                    (0.1, go(state, turn_right(action))),
+                    (0.1, go(state, turn_left(action)))]
+        else:
+            return [(0.0, state)]
+
+def turn_right(heading):
+    return turn_heading(heading, RIGHT)
+
+def turn_left(heading):
+    return turn_heading(heading, LEFT)
+
+def turn_heading(heading, inc, headings=orientations):
+    return headings[(headings.index(heading) + inc) % len(headings)]
+
+def go(state, direction):
+        state1 = vector_add(state, direction)
+        return state1 if state1 in states else state
+    
+def vector_add(a, b):
+    return tuple(map(operator.add, a, b))
+
+
+# Define MDP Class
+class MDP:
+
+    def __init__(self, init, actlist, terminals, transitions=None, reward=None, states=None, gamma=0.9):
+        if not (0 < gamma <= 1):
+            raise ValueError("An MDP must have 0 < gamma <= 1")
+
+        # collect states from transitions table if not passed.
+        self.states = states or self.get_states_from_transitions(transitions)
+            
+        self.init = init
+        
+        if isinstance(actlist, list):
+            # if actlist is a list, all states have the same actions
+            self.actlist = actlist
+
+        elif isinstance(actlist, dict):
+            # if actlist is a dict, different actions for each state
+            self.actlist = actlist
+        
+        self.terminals = terminals
+        self.transitions = transitions or {}
+        if not self.transitions:
+            print("Warning: Transition table is empty.")
+
+        self.gamma = gamma
+
+        self.reward = reward or {s: 0 for s in self.states}
+
+        # self.check_consistency()
+
+    def R(self, state):
+        """Return a numeric reward for this state."""
+
+        return self.reward[state]
+
+    def T(self, state, action):
+        """Transition model. From a state and an action, return a list
+        of (probability, result-state) pairs."""
+
+        if not self.transitions:
+            raise ValueError("Transition model is missing")
+        else:
+            return self.transitions[state][action]
+
+    def actions(self, state):
+        """Return a list of actions that can be performed in this state. By default, a
+        fixed list of actions, except for terminal states. Override this
+        method if you need to specialize by state."""
+
+        if state in self.terminals:
+            return [None]
+        else:
+            return self.actlist
+
+    def get_states_from_transitions(self, transitions):
+        if isinstance(transitions, dict):
+            s1 = set(transitions.keys())
+            s2 = set(tr[1] for actions in transitions.values()
+                     for effects in actions.values()
+                     for tr in effects)
+            return s1.union(s2)
+        else:
+            print('Could not retrieve states from transitions')
+            return None
+
+    def check_consistency(self):
+
+        # check that all states in transitions are valid
+        assert set(self.states) == self.get_states_from_transitions(self.transitions)
+
+        # check that init is a valid state
+        assert self.init in self.states
+
+        # check reward for each state
+        assert set(self.reward.keys()) == set(self.states)
+
+        # check that all terminals are valid states
+        assert all(t in self.states for t in self.terminals)
+
+        # check that probability distributions for all actions sum to 1
+        for s1, actions in self.transitions.items():
+            for a in actions.keys():
+                s = 0
+                for o in actions[a]:
+                    s += o[0]
+                assert abs(s - 1) < 0.001
+
+
+#Define Grid
+class Grid(MDP):
+    def __init__(self, grid, terminals, init=(0, 0), gamma=.9):
+        grid.reverse()     # because we want row 0 on bottom, not on top
+        reward = {}
+        states = set()
+        self.rows = len(grid)
+        self.cols = len(grid[0])
+        self.grid = grid
+        for x in range(self.cols):
+            for y in range(self.rows):
+                if grid[y][x]:
+                    states.add((x, y))
+                    reward[(x, y)] = grid[y][x]
+        self.states = states
+        actlist = orientations
+        transitions = {}
+        for s in states:
+            transitions[s] = {}
+            for a in actlist:
+                transitions[s][a] = self.calculate_T(s, a)
+        MDP.__init__(self, init, actlist=actlist,
+                     terminals=terminals, transitions=transitions, 
+                     reward=reward, states=states, gamma=gamma)
+
+    def calculate_T(self, state, action):
+        if action:
+            return [(0.8, self.go(state, action)),
+                    (0.1, self.go(state, turn_right(action))),
+                    (0.1, self.go(state, turn_left(action)))]
+        else:
+            return [(0.0, state)]
+    
+    def T(self, state, action):
+        return self.transitions[state][action] if action else [(0.0, state)]
+
+    def go(self, state, direction):
+        """Return the state that results from going in this direction."""
+
+        state1 = vector_add(state, direction)
+        return state1 if state1 in self.states else state
+
+    def to_grid(self, mapping):
+        """Convert a mapping from (x, y) to v into a [[..., v, ...]] grid."""
+
+        return list(reversed([[mapping.get((x, y), None)
+                               for x in range(self.cols)]
+                              for y in range(self.rows)]))
+
+    def to_arrows(self, policy):
+        chars = {(1, 0): '>', (0, 1): '^', (-1, 0): '<', (0, -1): 'v', None: '.'}
+        return self.to_grid({s: chars[a] for (s, a) in policy.items()})
+
